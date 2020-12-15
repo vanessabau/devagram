7d0c682a4fcc7ccae78dc30f83245e109be5a3db
Vanessa Bautista <vbautista916@icloud.com>
2020-12-15 06:55:13 -0800

    fixed a typo to create keys in map state to props

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,6 +2,7 @@ import { SET_ALERT, REMOVE_ALERT } from "./types";
 import { v4 as uuid } from "uuid";
 
 export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
+  //uuid gives us a universal id
   const id = uuid();
   dispatch({
     type: SET_ALERT,
